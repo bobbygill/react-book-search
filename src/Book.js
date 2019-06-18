5b548c1f615897171b2e36ce98642cd041e7c0ee
Bobby <38087760+bobbygill@users.noreply.github.com>
2019-06-18 13:16:22 -0700

    Adjusted spacing

--- src/Book.js
+++ src/Book.js
@@ -1,27 +1,25 @@
 import React from "react";
 
-
-function Book(props){
-
+function Book(props) {
   const Styles = {
     display: "inline-block",
-    border: '1px solid black',
+    border: "1px solid black",
     padding: 15,
     width: "55%"
   };
 
-return(
-  <div style={Styles}>
-  <span className="bold">Title:</span> <a href={props.url} target="_blank" rel="noopener noreferrer">{props.title}</a> <br />
-  <span className="bold">Category:</span> {props.category} <br />
-  <span className="bold">Description:</span> {props.description} <br />
-
-  {props.children}
-
-  </div>
-
-);
-
+  return (
+    <div style={Styles}>
+      <span className="bold">Title:</span>{" "}
+      <a href={props.url} target="_blank" rel="noopener noreferrer">
+        {props.title}
+      </a>
+      <br />
+      <span className="bold">Category:</span> {props.category} <br />
+      <span className="bold">Description:</span> {props.description} <br />
+      {props.children}
+    </div>
+  );
 }
 
 export default Book;
